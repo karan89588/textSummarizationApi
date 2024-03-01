8c4fbf407b15478cf497e60bc3c9043bc45b370b
karan89588 <karanmouryadmp@gmail.com>
2024-03-01 16:43:02 +0530

    adding_more

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from getSum import generateSummary
 from newsTitleGeneration import newsTitle
+from deep_translator import GoogleTranslator
+from langdetect import detect
+from extractiveSum import generateExtractiveSummary
 app=Flask(__name__)
 CORS(app)
 @app.route('/',methods=['GET'])
@@ -10,11 +13,27 @@ def get():
 
 @app.route('/getSummary',methods=['POST'])
 def getRes():
-	txt=request.json['txt']
-	print(txt)
-	output=generateSummary(txt)
-	return jsonify({'msg':'success','success':True,'sum':output})
-
+	try:
+		txt=request.json['txt']
+		target_lang=request.json['target_lang']
+		user_demand=request.json['user_demand']
+		if target_lang=='check':
+			try:
+				target_lang=detect(txt)
+				if target_lang!='en':
+					
+					txt=GoogleTranslator(source='auto',target='en').translate(txt)
+			except:
+				return jsonify({'msg':'Oppps Language Not allowed','success':False})
+		print(txt)
+		output=generateSummary(txt)
+		if user_demand!='':
+			target_lang=user_demand
+		if target_lang!='en':
+			output=GoogleTranslator(source='auto',target=target_lang).translate(output)
+		return jsonify({'msg':'success','success':True,'sum':output})
+	except:
+		return jsonify({'msg':'failed','success':False})
 @app.route('/getTitle',methods=['POST'])
 def getTitle():
 	txt=request.json['txt']
